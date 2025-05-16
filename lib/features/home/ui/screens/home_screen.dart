7b5a3681151ac13b32957a172c05e584699ba5fb
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2025-05-16 23:49:00 +0600

    home_screen design, home_carousel_slider, section_header

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:ecommerce_app/app/assets_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import '../widgets/app_bar_action_button.dart';
+import '../widgets/home_carousel_slider.dart';
+import '../widgets/section_header.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,25 +21,28 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextField(
-              textInputAction: TextInputAction.search,
-              decoration: InputDecoration(
-              fillColor: Colors.grey.shade200,
-              filled: true,
-              hintText: "Search for products",
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderSide: BorderSide.none
-              ),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide.none
-              ),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide.none
-              ),
-            ),)
+            _buildSearchTextField(),
+            const SizedBox(height: 16),
+            HomeCarouselSlider(),
+            const SizedBox(height: 16),
+            SectionHeader(title: 'Categories', onTapSeeAll: () {}),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSearchTextField() {
+    return TextField(
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        fillColor: Colors.grey.shade200,
+        filled: true,
+        hintText: "Search for products",
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(borderSide: BorderSide.none),
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
       ),
     );
   }
