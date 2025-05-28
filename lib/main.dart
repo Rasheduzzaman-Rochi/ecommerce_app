b954eb91509a0d324b5f9eec821eac783694f4f1
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2025-05-29 02:14:38 +0600

    updated some files

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
-import "package:ecommerce_app/firebase_options.dart";
-import "package:firebase_core/firebase_core.dart";
-import "package:firebase_crashlytics/firebase_crashlytics.dart";
-import "package:flutter/foundation.dart";
-import "package:flutter/material.dart";
-import "app/app.dart";
+import 'package:ecommerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';  // Import flutter_dotenv
+import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform);
 
-  // framework error
+  // Load environment variables from .env file
+  await dotenv.load();
+
+  // Initialize Firebase with options based on platform
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Set up Firebase Crashlytics to handle framework errors
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
   };
@@ -20,5 +27,6 @@ Future<void> main() async {
     return true;
   };
 
+  // Run the app
   runApp(const CraftyBay());
 }
